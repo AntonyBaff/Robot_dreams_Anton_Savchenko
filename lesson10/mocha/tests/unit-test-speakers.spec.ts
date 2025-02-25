2de2a23ee27c71a52d8ef061e28246c7d1b4b9ca
Antony <savchenko3337@gmail.com>
2025-02-25 21:10:00 +0200

    Homework 10

---
+++ lesson10/mocha/tests/unit-test-speakers.spec.ts
@@ -0,0 +1,60 @@
+import { expect } from 'chai';
+import { BluetoothHeadphones as Speakers } from 'src/bluetooth-headphones';
+
+describe('Unit test suite for class Speakers', () => {
+    it('1 Check changeVolume method', () => {
+        // Arrange
+        const obj = new Speakers();
+
+        //Act
+        obj.changeVolume(50);
+
+        //Assert
+        expect(obj.volumeControl).to.be.equal(50);
+
+    });
+    it('2 Check turnOn method', () => {
+        // Arrange
+        const obj = new Speakers();
+
+        //Act
+        obj.turnOn();
+
+        //Assert
+        expect(obj.status).to.be.equal(true);
+
+    });
+    it('3. Check turnOff method', () => {
+        // Arrange
+        const obj = new Speakers();
+
+        //Act
+        obj.turnOff();
+
+        //Assert
+        expect(obj.status).to.be.equal(false);
+
+    });
+    it('4. Check turnOff method LED indicator', () => {
+        // Arrange
+        const obj = new Speakers();
+
+        //Act
+        obj.turnOff();
+
+        //Assert
+        expect(obj.currentIndicator).to.be.equal('Red');
+
+    });
+    it('5. Check turnOn method LED indicator', () => {
+        // Arrange
+        const obj = new Speakers();
+
+        //Act
+        obj.turnOn();
+
+        //Assert
+        expect(obj.currentIndicator).to.be.equal('Green');
+
+    });
+});
